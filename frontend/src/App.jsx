9a742651e9523cdb7f9caa44cd8ea1149300a8ba
Gladdy17 <gladston.bosco@gmail.com>
2025-02-09 10:41:36 -0700

    Display 3 different Photos

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
 
 const sampleDataForPhotoListItem = {
-  id: "1",
+  id: "1",  // This is the sample id, but we'll override it in each item for uniqueness.
   location: {
     city: "Montreal",
     country: "Canada",
@@ -13,11 +13,28 @@ const sampleDataForPhotoListItem = {
   profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
 };
 
-const App = () => (
-  <div className="App">
-    <PhotoListItem {...sampleDataForPhotoListItem} />
-  </div>
-);
+const App = () => {
+  // Create an array with 3 items.
+  const photos = new Array(3).fill(null);
+
+  return (
+    <div className="App">
+      <div className="photo-list">
+        {photos.map((_, index) => (
+          <PhotoListItem
+            key={index}                   // 'key' is a reserved prop in React; it should be unique for each element.
+            photoId={index + 1}           // Use a unique photoId for each photo; here we simply use index + 1.
+            imageSource={sampleDataForPhotoListItem.imageSource}
+            username={sampleDataForPhotoListItem.username}
+            location={sampleDataForPhotoListItem.location}
+            profile={sampleDataForPhotoListItem.profile}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default App;
+
 
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -5,13 +5,14 @@ import React from 'react';
 
 const PhotoListItem = ({ photoId, imageSource, username, location, profile }) => {
   return (
-    <div className="photo-list-item">
-      <img src={imageSource} alt={`Photo by ${username}`} />
-      <div>{username}</div>
-      <div>{location.city}, {location.country}</div>
-      <img src={profile} alt={`${username}'s profile`} />
+    <div className="photo-list__item">
+      <img className="photo-list__image" src={imageSource} alt={`Photo by ${username}`} />
+      <div className="photo-list__user-info">{username}</div>
+      <div className="photo-list__user-location">{location.city}, {location.country}</div>
+      <img className="photo-list__user-profile" src={profile} alt={`${username}'s profile`} />
     </div>
   );
+
 };
 
 export default PhotoListItem;
