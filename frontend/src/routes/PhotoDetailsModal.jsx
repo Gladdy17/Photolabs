5b577897a1daef17a1bc678a7725cee2b4d2cd04
Gladdy17 <gladston.bosco@gmail.com>
2025-02-16 17:13:22 -0700

    Added images to modal

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -23,6 +23,9 @@ const PhotoDetailsModal = ({ photo, onClose, favourites, setFavourites }) => {
 
   const isFav = favourites.includes(photo.id);
 
+  // Convert similar_photos object to an array (if it exists)
+  const similarPhotos = photo.similar_photos ? Object.values(photo.similar_photos) : [];
+
   return (
     <div className="photo-details-modal" onClick={handleOverlayClick}>
       <div className="photo-details-modal__content">
@@ -33,13 +36,34 @@ const PhotoDetailsModal = ({ photo, onClose, favourites, setFavourites }) => {
           <img src={closeSymbol} alt="Close symbol" />
         </button>
         <div className="photo-details-modal__body">
-          {/* Placeholder for modal content */}
-          <div className="photo-details-modal__icon">+</div>
-          {/* Favourite icon in the modal */}
-          <PhotoFavButton
-            onClick={(e) => { e.stopPropagation(); toggleFavourite(); }}
-            isFav={isFav}
-          />
+          {/* Display the larger version of the selected photo */}
+          <div className="photo-details-modal__large-photo">
+            <img
+              src={photo.urls.full}
+              alt={`Large photo by ${photo.user.name}`}
+            />
+          </div>
+          {/* Favourite button in the modal */}
+          <div className="photo-details-modal__favourite">
+            <PhotoFavButton
+              onClick={(e) => { e.stopPropagation(); toggleFavourite(); }}
+              isFav={isFav}
+            />
+          </div>
+          {/* Display similar photos below */}
+          <div className="photo-details-modal__similar-photos">
+            <h3>Similar Photos</h3>
+            <div className="photo-details-modal__similar-list">
+              {similarPhotos.map(simPhoto => (
+                <div key={simPhoto.id} className="photo-details-modal__similar-item">
+                  <img
+                    src={simPhoto.urls.regular}
+                    alt={`Similar photo by ${simPhoto.user.name}`}
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -47,6 +71,7 @@ const PhotoDetailsModal = ({ photo, onClose, favourites, setFavourites }) => {
 };
 
 export default PhotoDetailsModal;
+
 
 
 
